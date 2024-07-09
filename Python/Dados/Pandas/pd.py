e5fd2a37c035811ae658223f1e09cc429fe8f0b7
Ulisses <ulisseskelermann@gmail.com>
2024-07-08 22:02:45 -0300

    Avancei mais nos estudos

--- Python/Dados/Pandas/pd.py
+++ Python/Dados/Pandas/pd.py
@@ -1,7 +1,7 @@
 import pandas
 
 dados = {"Marca" : ["Chevrolet", "Ford", "Volkswagen"],
-         "Ano"
+         "Ano" : [1971, 1980, 1998],
          "Modelo" : ["Opala", "Maverick", "Gol"],
          "Motor" : ["I6", "V8", "AP"],
          "Cilindrada" : [4.1, 4.9, 2.2]}
@@ -20,3 +20,10 @@ print(df["Modelo"], "\n")
         #Coloca dois colchetes pois esta passando uma lista para o index
 print(df[ ["Motor", "Cilindrada"] ], "\n")
 
+
+print(df[1:3], "\n")#Vai pegar do elemento de index 1 até o elemento de index 3
+
+df = pandas.DataFrame(dados,  
+                      columns = ["Marca", "Ano", "Modelo", "Cilindrada", "Motor", "Dono", "Multas"])
+
+print(df, "\n")
